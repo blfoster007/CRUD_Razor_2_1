2e2f4759ccb6394a2f19bebc558e340b8d867ea3
Brent Foster <brent_foster@ymail.com>
2018-09-10 00:13:01 -0500

    Next set of bulk changes.

--- CRUD_Razor_2_1/Pages/BookList/Create.cshtml.cs
+++ CRUD_Razor_2_1/Pages/BookList/Create.cshtml.cs
@@ -12,6 +12,9 @@ namespace CRUD_Razor_2_1.Pages.BookList
     {
         private readonly ApplicationDbContext _db;
 
+        [TempData]
+        public string Message { get; set; }
+
         public CreateModel(ApplicationDbContext db)
         {
             _db = db;
@@ -25,7 +28,7 @@ namespace CRUD_Razor_2_1.Pages.BookList
 
         }
 
-        public async Task<IActionResult> OnPost(Book Book)
+        public async Task<IActionResult> OnPost()
         {
             if(!ModelState.IsValid)
             {
@@ -34,7 +37,7 @@ namespace CRUD_Razor_2_1.Pages.BookList
 
             _db.Books.Add(Book);
             await _db.SaveChangesAsync();
-
+            Message = "Book has been created successfully!";
             return RedirectToPage("Index");
         }
     }
